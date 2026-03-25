ebf7e887bb5be4bae6ce73dd76802e2b7db6cf3e
Robert Miron <73788820+Thoud@users.noreply.github.com>
2026-03-25 19:31:16 +0100

    Remove service worker and update imports

--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.css';
-import './home.css'
-import './stresstask.css'
-import './imprint.css'
-import './i18n';
+import './locales/i18n';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import './css/main.css';
 
 // Append app to dom
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById('root'));
