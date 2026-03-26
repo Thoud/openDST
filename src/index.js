0c41193c64821959a4ea8f2648ee42f92fb0b3c1
Robert Miron <73788820+Thoud@users.noreply.github.com>
2026-03-26 21:57:44 +0100

    Add service worker functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import './locales/i18n';
 
 import 'bootstrap/dist/css/bootstrap.css';
@@ -7,3 +8,4 @@ import './css/main.css';
 
 // Append app to dom
 ReactDOM.render(<App />, document.getElementById('root'));
+serviceWorkerRegistration.register();
